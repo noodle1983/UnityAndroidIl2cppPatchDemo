6d1ebf1aacd8930440712f6e71c625bb05323eb3
LuDong <noodle1983@126.com>
2018-11-08 11:48:10 +0800

    change for version2

--- Assets/AndroidIl2cppPatchDemo/Editor/AndroidBuilder.cs
+++ Assets/AndroidIl2cppPatchDemo/Editor/AndroidBuilder.cs
@@ -125,7 +125,7 @@ public class AndroidBuilder : MonoBehaviour {
 
         //export project
         string error_msg = string.Empty;
-        string[] levels = new string[] { "Assets/AndroidIl2cppPatchDemo/Scene/0.unity" };
+        string[] levels = new string[] { "Assets/AndroidIl2cppPatchDemo/Scene/0.unity", "Assets/AndroidIl2cppPatchDemo/Scene/1.unity" };
         BuildOptions options = BuildOptions.None;
         EditorUserBuildSettings.exportAsGoogleAndroidProject = true;
         if (Directory.Exists(ANDROID_EXPORT_PATH)) { FileUtil.DeleteFileOrDirectory(ANDROID_EXPORT_PATH);}
@@ -295,7 +295,7 @@ import io.github.noodle1983.Boostrap;");
         allZipCmds.AppendFormat("cd {0} && echo assets/bin/Data/to_be_removed1.txt> {1}\n", patchTopPath, TO_BE_REMOVED_FILE_NAME);
         allZipCmds.AppendFormat("cd {0} && echo assets/bin/Data/to_be_removed2.txt>> {1}\n", patchTopPath, TO_BE_REMOVED_FILE_NAME);
 
-        string zippedPatchFile = PROJECT_DIR + "/Assets/AndroidIl2cppPatchDemo/PrebuiltPatches/AllAndroidPatchFiles_Version1.zip";
+        string zippedPatchFile = PROJECT_DIR + "/Assets/AndroidIl2cppPatchDemo/PrebuiltPatches/AllAndroidPatchFiles_Version2.zip";
         if (File.Exists(zippedPatchFile)) { FileUtil.DeleteFileOrDirectory(zippedPatchFile);  }
         allZipCmds.AppendFormat("cd {0} && {1} -9 -r \"{2}\" \"{3}\"\n", patchTopPath, ZIP_PATH, zippedPatchFile, "*");
         allZipCmds.AppendFormat("explorer.exe {0} \n\n", (PROJECT_DIR + "/Assets/AndroidIl2cppPatchDemo/PrebuiltPatches/").Replace("//", "/").Replace("/", "\\"));
