de464ef724ea262695c7363fc14e89fa25fc4b04
LuDong <noodle1983@126.com>
2018-11-08 11:26:42 +0800

    add build cmd and ignore

---
+++ build_demo_apk.cmd
@@ -0,0 +1,22 @@
+rem global
+SET UnityBin="C:\Program Files\Unity\Editor\Unity.exe"
+
+rem prepare build cmd
+SET ScriptPath=%~dp0
+SET ScriptPath=%ScriptPath:~0,-1%
+
+rem ==========================================================================================
+rem build base apk
+rem ==========================================================================================
+@echo "start to build base version apk"
+SET ProjectPath=%ScriptPath%
+SET BuildCmd=%UnityBin% -quit -buildTarget android -batchmode -projectPath "%ProjectPath%" 
+
+if not exist "%ProjectPath%\AndroidGradleProject\Test\src\main\bin\com.test.test.apk" (
+    echo "Build Version1 Failed! Please Rerun %ProjectPath%\AndroidGradleProject\Test\src\main\build_apk.bat to check the error."
+	exit -1
+)
+
+copy /Y %ProjectPath%\AndroidGradleProject\Test\src\main\bin\com.test.test.apk  %ScriptPath%\Il2cppDemo_com.test.test.apk
+echo "Done!"
+exit 0
