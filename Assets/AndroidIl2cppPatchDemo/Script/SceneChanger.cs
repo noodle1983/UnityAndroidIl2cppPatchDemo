3a54c147572173d0716c48d207ba470b1000e769
LuDong <noodle1983@126.com>
2018-11-08 11:48:10 +0800

    change for version2

---
+++ Assets/AndroidIl2cppPatchDemo/Script/SceneChanger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+    public string sceneName = "0";
+    public MessageBoxUI messageBox;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void OnClickLoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+    
+}
