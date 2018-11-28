7953d9adb83e2d99af528e4a6eb38600accc58df
LuDong <noodle1983@126.com>
2018-11-28 22:47:10 +0800

    add build all command

--- build_demo_apk.cmd
+++ build_demo_apk.cmd
@@ -5,14 +5,46 @@ rem prepare build cmd
 SET ScriptPath=%~dp0
 SET ScriptPath=%ScriptPath:~0,-1%
 
+SET ProjectPath=%ScriptPath%
+SET BuildCmd=%UnityBin% -quit -buildTarget android -batchmode -projectPath "%ProjectPath%" 
+
+rem ==========================================================================================
+rem build version1 patch
+rem ==========================================================================================
+@echo "start to build version1"
+
+cd %ProjectPath%
+git checkout version1
+del %ProjectPath%\Assets\AndroidIl2cppPatchDemo\PrebuiltPatches\AllAndroidPatchFiles_Version1.zip
+%BuildCmd% -executeMethod AndroidBuilder.BuildPatch
+if not exist "%ProjectPath%\Assets\AndroidIl2cppPatchDemo\PrebuiltPatches\AllAndroidPatchFiles_Version1.zip" (
+    echo "Build Version1 Failed!"
+	pause
+	exit -1
+)
+
+rem ==========================================================================================
+rem build version2 patch
+rem ==========================================================================================
+@echo "start to build version2"
+
+cd %ProjectPath%
+git checkout version2
+del %ProjectPath%\Assets\AndroidIl2cppPatchDemo\PrebuiltPatches\AllAndroidPatchFiles_Version2.zip
+%BuildCmd% -executeMethod AndroidBuilder.BuildPatch
+if not exist "%ProjectPath%\Assets\AndroidIl2cppPatchDemo\PrebuiltPatches\AllAndroidPatchFiles_Version2.zip" (
+    echo "Build Version2 Failed!"
+	pause
+	exit -1
+)
+
 rem ==========================================================================================
 rem build base apk
 rem ==========================================================================================
 @echo "start to build base version apk"
-SET ProjectPath=%ScriptPath%
-SET BuildCmd=%UnityBin% -quit -buildTarget android -batchmode -projectPath "%ProjectPath%" 
 
 cd %ProjectPath%
+git checkout master
 %BuildCmd% -executeMethod AndroidBuilder.BuildWithoutPatch
 if not exist "%ProjectPath%\AndroidGradleProject\Test\src\main\bin\com.test.test.apk" (
     echo "Build Failed! Please Rerun %ProjectPath%\AndroidGradleProject\Test\src\main\build_apk.bat to check the error."
