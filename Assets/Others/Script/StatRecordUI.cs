8f7c6ac614ff35b0c149cebe57e30294e46cf2f7
LuDong <noodle1983@126.com>
2019-07-27 23:48:00 +0800

    show stat record

---
+++ Assets/Others/Script/StatRecordUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatRecordUI : MonoBehaviour
+{
+    public Text phoneType;
+    public Text enterScene;
+
+    StatRecord statRecord = null;
+    void Start()
+    {
+        RefreshUI();
+    }
+
+    void RefreshUI()
+    {
+        if (statRecord == null)
+        {
+            phoneType.text = string.Empty;
+            enterScene.text = string.Empty;
+            return;
+        }
+
+        phoneType.text = statRecord.phone_type + " (" + statRecord.android_version + ")";
+        int enterSceneBits = statRecord.enter_scene;
+        StringBuilder enterSceneStr = new StringBuilder();
+        for (int i = 0; i < 3; i++)
+        {
+            if ((enterSceneBits & (1 << i)) != 0)
+            {
+                enterSceneStr.Append(i.ToString() + ",");
+            }
+        }
+        if (enterSceneStr.Length > 0) { enterSceneStr.Remove(enterSceneStr.Length - 1, 1); }
+        enterScene.text = enterSceneStr.ToString();
+
+        enterScene.color = enterSceneBits == 7 ? Color.green : Color.yellow;
+        phoneType.color = enterScene.color;
+    }
+
+    public void ResetRecord(StatRecord record)
+    {
+        this.statRecord = record;
+        if (phoneType == null)
+        {
+            return;
+        }
+
+        RefreshUI();
+    }
+}
---
+++ Assets/Others/Script/StatRecordUIMgr.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatRecordUIMgr : MonoBehaviour
+{
+    public GridLayoutGroup gridLayoutGroup;
+    public StatRecordUI templateUI;
+
+    List<StatRecordUI> statRecoredUIList = new List<StatRecordUI>();
+    List<StatRecord> statRecordList = new List<StatRecord>();
+
+    void Start()
+    {
+        statRecoredUIList.Add(templateUI);
+        StartCoroutine(SupportPhoneStat.FetchSupportedStat(OnGotRecordData));
+        RefreshUI();
+        
+    }
+
+    void OnGotRecordData(StatRsp rsp)
+    {
+        if (rsp == null)
+        {
+            statRecordList.Clear();
+            RefreshUI();
+            return;
+        }
+
+        //Debug.Log(Newtonsoft.Json.JsonConvert.SerializeObject(rsp));
+        statRecordList = rsp.data;
+        RefreshUI();
+
+    }
+
+    void RefreshUI()
+    {
+        statRecordList.Sort((a, b) => a.enter_scene.CompareTo(b.enter_scene));
+
+        int recordIndex = 0;
+        for(; recordIndex < statRecordList.Count; recordIndex++)
+        {
+            if (recordIndex >= statRecoredUIList.Count)
+            {
+                GameObject newObject = Object.Instantiate<GameObject>(templateUI.gameObject, gridLayoutGroup.transform);
+                StatRecordUI recordUI = newObject.GetComponent<StatRecordUI>();
+                statRecoredUIList.Add(recordUI);
+            }
+            StatRecordUI statRecordUI = statRecoredUIList[recordIndex];
+            statRecordUI.ResetRecord(statRecordList[recordIndex]);
+            statRecordUI.gameObject.SetActive(true);
+        }
+
+        for(; recordIndex < statRecoredUIList.Count; recordIndex++)
+        {
+            statRecoredUIList[recordIndex].gameObject.SetActive(false) ;
+        }
+
+        //gridLayoutGroup.
+    }
+}
