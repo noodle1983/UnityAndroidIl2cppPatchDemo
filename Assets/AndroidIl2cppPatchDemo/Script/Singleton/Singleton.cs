28768cdfe7d7edfd91f82c57322f79880fbb06b2
LuDong <noodle1983@126.com>
2019-12-16 21:45:32 +0800

    add and show thread info

---
+++ Assets/AndroidIl2cppPatchDemo/Script/Singleton/Singleton.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+	private static T instance;
+	private static object @lock = new object();
+
+	public static T Instance
+	{
+		get
+		{
+			lock (@lock)
+			{
+				if (instance == null)
+				{
+					instance = (T) FindObjectOfType(typeof(T));
+					
+					if ( FindObjectsOfType(typeof(T)).Length > 1 )
+					{
+						Debug.LogError("[Singleton] Something went really wrong " +
+						               " - there should never be more than 1 singleton!" +
+						               " Reopenning the scene might fix it.");
+						return instance;
+					}
+					
+					if (instance == null)
+					{
+#if UNITY_EDITOR
+						GameObject singleton =  EditorUtility.CreateGameObjectWithHideFlags("", HideFlags.DontSave);
+#else
+						GameObject singleton = new GameObject();
+#endif
+						instance = singleton.AddComponent<T>();
+						singleton.name = "(singleton) "+ typeof(T).ToString();
+						DontDestroyOnLoad(singleton);
+
+					} 
+				}
+				return instance;
+			}
+		}
+	}
+}
---
+++ Assets/Others/Script/ThreadInfoUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ThreadInfoUI : MonoBehaviour
+{
+    public Text index;
+    public Text desc;
+    int id;
+    BackgroundThread backgroundThread;
+
+    private void Start()
+    {
+        this.index.text = id.ToString() + ".";
+
+    }
+
+    void Update()
+    {
+        if (backgroundThread == null || desc == null)
+        {
+            return;
+        }
+
+        desc.text = backgroundThread.GetRunningJobName() ;      
+    }
+
+    public void ResetRecord(int id, BackgroundThread backgroundThread)
+    {
+        this.id = id;
+        this.backgroundThread = backgroundThread;
+        if (desc == null)
+        {
+            return;
+        }
+        this.index.text = id.ToString() + ".";
+    }
+}
