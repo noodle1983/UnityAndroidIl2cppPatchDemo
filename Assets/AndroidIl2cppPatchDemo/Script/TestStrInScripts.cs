3e30d73aa9aa62b0650ec9f633d293b094c42635
LuDong <noodle1983@126.com>
2020-07-12 16:13:23 +0800

    test string in scripts

---
+++ Assets/AndroidIl2cppPatchDemo/Script/TestStrInScripts.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestStrInScripts : MonoBehaviour
+{
+    public Text text;
+    const string t = "string in scripts: base version";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.text = t;
+    }
+}
